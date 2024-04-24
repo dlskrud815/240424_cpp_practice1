d2c5a6a0f349200d2f7950d0e11749d9cbb1e27f
dlskrud815 <dlskrud815@gmail.com>
2024-04-24 15:37:47 +0900

    Add project files.

---
+++ 240424_cpp_practice1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+//함수명은 대문자로 시작
+int GetTen() //int형 변수 리턴
+{
+	return 10; //int 10 리턴
+}
+
+int IntSum(int n1, int n2)
+{
+	return n1 + n2;
+}
+
+
+void PrintSomething(string message) //void형 ruturn x
+{
+	cout << message << endl;
+}
+
+int main() //cout 처리는 main문에서 처리하는 것이 이상적
+{
+	int num1, num2;
+
+	cin >> num1 >> num2;
+
+	int ten = GetTen(); //ten = 10;
+	int result = IntSum(num1, num2);
+
+	PrintSomething("안녕하세요:)");
+
+	return 0;
+}
---
+++ Exercise1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int Add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+int Sub(int num1, int num2)
+{
+	if (num1 >= num2)
+	{
+		return num1 - num2;
+	}
+	else
+	{
+		return num2 - num1;
+	}
+}
+
+int Mul(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+float Divide(int num1, int num2)
+{
+	return (float)num1 / (float)num2; //강제 형변환
+}
+
+int main()
+{
+	int num1, num2;
+
+	cin >> num1 >> num2;
+
+	cout << "Add: " << Add(num1, num2) << endl;
+	cout << "Sub: " << Sub(num1, num2) << endl;
+	cout << "Mul: " << Mul(num1, num2) << endl;
+	cout << "Divide: " << Divide(num1, num2) << endl;
+
+	return 0;
+}
