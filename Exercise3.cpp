ba7f1db694a8f1d236e51d9706ad48be4509f069
dlskrud815 <dlskrud815@gmail.com>
2024-04-24 17:45:20 +0900

    cout 추가

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -6,6 +6,7 @@ int main()
 	string input;
 	string city[] = { "미국", "영국", "한국", "일본", "중국" };
 
+	cout << "list를 입력해 나라를 확인하세요.";
 	cin >> input;
 
 	if (input == "list")
@@ -15,21 +16,21 @@ int main()
 			cout << country << endl;
 		}
 	}
-
-	for (int i = 0; i < sizeof(city) / sizeof(city[0]); i++)
-	{
-		if (input == city[i])
+	else {
+		for (int i = 0; i < sizeof(city) / sizeof(city[0]); i++)
 		{
-			cout << "인덱스: " << i << ", 나라이름: " << city[i] << endl;
-			break;
-		}
-		else
-		{
-			cout << "오류! 해당 나라이름은 존재하지 않습니다." << endl;
-			break;
+			if (input == city[i])
+			{
+				cout << "인덱스: " << i << ", 나라이름: " << city[i] << endl;
+				break;
+			}
+			else
+			{
+				cout << "오류! 해당 나라이름은 존재하지 않습니다." << endl;
+				break;
+			}
 		}
 	}
-
 
 	return 0;
 }
